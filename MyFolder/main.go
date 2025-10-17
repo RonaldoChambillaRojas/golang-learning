f34523f69934d617ed5b621a1a94cfff43a5fcf0
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-16 21:54:10 -0500

    feat: add MyFolder && varable declaration (var , const) in numbers

---
+++ MyFolder/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Declaracion de contantes.
+	const pi float64 = 3.14
+	const pi2 = 3.1416
+
+	fmt.Println("pi", pi)
+	fmt.Println("pi2", pi2)
+
+	// Declaracion de Varibles enteras.
+	base := 12
+	var altura int = 14
+	var area int
+	var otra = 2
+
+	fmt.Println(base, altura, area, otra)
+}
