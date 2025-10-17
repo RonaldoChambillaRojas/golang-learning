67c6133d0ee50778ecfe3f9baaa80bc6279d68fe
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-16 23:42:24 -0500

    feat: (fmt) print type of variable

--- MyFmt/main.go
+++ MyFmt/main.go
@@ -22,4 +22,8 @@ func main() {
 	message := fmt.Sprintf("%s tiene mas de %d cursos\n", nombre, cursos)
 	fmt.Println(message)
 
+	// Tipo datos
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("cursos: %T\n", cursos)
+
 }
