8a64b3b40013b7e9e88c51a8b72a0745fa2fd80f
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-10-20 16:08:51 -0500

    docs: (palindromos) coments

--- Polindromos/main.go
+++ Polindromos/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func isPalindromo(texto string) {
+
+	// ? CONFIRMAR:
+	// ? esto lo copie de un compañero, algo que voy notando esque a diferencia de js-
+	// ? que tenemos las propidades y metodos dentro de cada tipo de dato, aqui en go-
+	// ? me da la secacion de que estos metodos como que estan separados de los tipos-
+	// ? de datos, o algo asi.
+	textoMin := strings.ToLower(texto)
+
 	var textRevese string
-	for i := len(texto) - 1; i >= 0; i-- {
-		textRevese += string(texto[i])
+	for i := len(textoMin) - 1; i >= 0; i-- {
+
+		// ? CONFIRMAR:
+		//? ??: Esta parte me parece muy interesante, se PARESE a la concatenacion de javascript-
+		// ? cuando sumamos dos cadenas de texto, pero a la ves no da la sensacion de que sea lo mismo.
+		// ? QUE PASA AQUI??
+		textRevese += string(textoMin[i])
 	}
 
 	if texto == textRevese {
