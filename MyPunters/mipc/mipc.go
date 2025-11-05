05b99c33bb454b4023367e5db74c729dc45f4a8d
RonaldoChambillaRojas <ronaldochambillarojas@gmail.com>
2025-11-04 19:20:41 -0500

    feat: (app) use Stringers

--- MyPunters/mipc/mipc.go
+++ MyPunters/mipc/mipc.go
@@ -15,3 +15,7 @@ func (myPC Pc) Ping() { //? Aqui no entendi muy bien esta forma de declarar la f
 func (myPC *Pc) DuplicateRAM() {
 	myPC.Ram = myPC.Ram * 2
 }
+
+func (myPC Pc) String() string {
+	return fmt.Sprintf("Tengo %d GB RAM, %d DB Disco y es una %s", myPC.Ram, myPC.Disk, myPC.Brand)
+}
